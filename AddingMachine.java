2c392c8beb2365d6c1f7a2d5feb857c0a5adf1aa
eajenner <55466151+eajenner@users.noreply.github.com>
2019-10-09 15:56:10 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -3,28 +3,33 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	public AddingMachine () {
+		history = "0";
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		history = history + "+" + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value;
+		history = history + "-" + value;		
 	}
 		
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
-	
+		total = 0;
+		history = "0";
 	}
 }
